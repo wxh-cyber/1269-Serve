b56c94e4a2bb01d8099cf505b3cfddc0c65257bd
风雪心 <1097071510@qq.com>
2026-05-17 22:30:09 +0800

    feat(OneNet): 增加了组件内容

--- pages/OneNet/OneNet.vue
+++ pages/OneNet/OneNet.vue
@@ -20,18 +20,30 @@
 				<view class="event-online-button-wrap">
 					<!-- 申报在线办理容器 -->
 					<view class="apply-online-wrap">
+						<!-- 申报在线办理背景按钮图 -->
 						<image src="/static/OneNet/申报在线办理按钮.png" class="apply-online-bg" />
+						<text class="apply-online-text">申报在线办理</text>
 					</view>
 					
-					<!--  -->
-					<view class="">
-						
+					<!-- 事项办件查询容器 -->
+					<view class="event-item-search-wrap">
+						<!-- 事项办件查询背景按钮图 -->
+						<image src="/static/OneNet/事项办件查询按钮.png" class="event-item-search-bg" />
+						<text class="event-item-search-text">事项办件查询</text>
 					</view>
 				</view>
 			</view>
 			
 			<!-- 我的代办 -->
 			<view class="my-plan">
+				<!-- 我的代办背景图片 -->
+				<image class="my-plan-img" mode="aspectFill" />
+				
+				<text class="my-plan-person">我的办件</text>
+				<text class="my-plan-enterpise">企业服务一网通办</text>
+				<view class="click-enter-wrap">
+					<image src="/static/OneNet/" />
+				</view>
 				
 			</view>
 		</view>
@@ -91,7 +103,7 @@
 .cards-container {
 	display:flex;
 	flex-direction: column;
-	justify-content: space-between;
+	justify-content: center;
 	align-items: center;
 	gap:30rpx;
 }
@@ -104,6 +116,10 @@
 	box-sizing:border-box;
 	border:1px solid #fff;
 	border-radius:16rpx;
+	
+	display:flex;
+	flex-direction:column;
+	justify-content: center;
 	
 	overflow:hidden;
 	
@@ -121,7 +137,6 @@
 
 /* 事项在线办理文本 */
 .event-online-text {
-	position:absolute;
 	display:block;
 	margin-top:50rpx;
 	margin-left:42rpx;
@@ -138,7 +153,11 @@
 .event-online-button-wrap {
 	display:flex;
 	flex-direction: row;
-	justify-content: center;
+	/* 从最左侧横向排列 */
+	justify-content: flex-start;
+	gap:20rpx;
+	
+	margin-bottom:30rpx;
 }
 
 /* 申报在线办理容器 */
@@ -146,7 +165,13 @@
 	position:relative;
 	width:260rpx;
 	height:54rpx;
+	margin-left:42rpx;
 	border-radius:999rpx;
+	
+	display:flex;
+	flex-direction:row;
+	justify-content:center;
+	align-items: center;
 }
 
 /* 申报在线办理按钮图片 */
@@ -157,4 +182,103 @@
 	height:100%;
 	z-index:1;
 }
+
+/* 申报在线办理文本 */
+.apply-online-text {
+	display:inline-block;
+	margin-right:15rpx;
+	
+	font-size:28rpx;
+	font-weight:500;
+	color:#fff;
+	z-index:2;
+}
+
+/* 事项办件查询容器 */
+.event-item-search-wrap {
+	position:relative;
+	width:260rpx;
+	height:54rpx;
+	border-radius:999rpx;
+	
+	display:flex;
+	flex-direction:row;
+	justify-content: center;
+	align-items: center;
+}
+
+/* 事项办件查询背景 */
+.event-item-search-bg {
+	position:absolute;
+	inset:0;
+	width:100%;
+	height:100%;
+	z-index:1;
+}
+
+/* 事项办件查询文本 */
+.event-item-search-text {
+	display:inline-block;
+	margin-right:15rpx;
+	
+	font-size:28rpx;
+	font-weight:500;
+	color:#058b52;
+	z-index:2;
+}
+
+/* 我的代办外层容器 */
+.my-plan {
+	position:relative;
+	display:flex;
+	flex-direction:column;
+	justify-content: center;
+	gap:18rpx;
+	
+	width:690rpx;
+	height:170rpx;
+	border-radius:16rpx;
+	box-sizing: border-box;
+	
+	overflow:hidden;
+	
+	box-shadow:0 10rpx 10rpx 0 rgba(0,0,0,0.1);
+}
+
+/* 我的代办内层背景图片 */
+.my-plan-img {
+	position:absolute;
+	inset:0;
+	width:100%;
+	height:100%;
+	z-index:1;
+}
+
+/* 我的办件文本 */
+.my-plan-person {
+	margin-left:42rpx;
+	
+	font-size:32rpx;
+	font-weight:bold;
+	color:#fff;
+}
+
+/* 企业服务一网通办 */
+.my-plan-enterpise {
+	margin-left:42rpx;
+	
+	font-size:24rpx;
+	font-weight: normal;
+	color:#fff;
+}
+
+/* 点击进入外部容器 */
+.click-enter-wrap {
+	position:relative;
+	margin-left:42rpx;
+	width:262rpx;
+	height:56rpx;
+}
+
+
 </style>
